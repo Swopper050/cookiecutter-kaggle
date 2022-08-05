bbb1e1ff988fce8ebd2f7f31c76e397ec14ac526
Swopper050 <bram@witoos.nl>
2022-08-05 19:35:26 +0200

    Added linters and basic script

---
+++ {{cookiecutter.repository_name}}/inspect_models.py
@@ -0,0 +1,11 @@
+import argparse
+
+
+def main(args):
+    print("TODO: load models and plot predictions/performance")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+    main(args)
